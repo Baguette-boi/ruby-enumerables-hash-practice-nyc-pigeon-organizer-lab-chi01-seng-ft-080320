4e218b5fee22e165ddbb65f663c9762af3d5dc4a
Victor Manzanilla <vmanzanilla7@gmail.com>
2020-07-27 00:27:10 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -8,9 +8,9 @@ def nyc_pigeon_organizer(data)
         new_array[name] = {}
       end
       if !new_array[name][k]
-        !new_array[name][k] = {}
+        !new_array[name][k] = []
       end
-      new_array[name][k].push{inner_k.to_s}
+      new_array[name][k].push(inner_k.to_s)
       end
     end
   end
