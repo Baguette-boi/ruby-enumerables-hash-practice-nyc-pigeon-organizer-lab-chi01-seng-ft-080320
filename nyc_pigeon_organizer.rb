6cbdfb71b8fb181cfb746de2b187142082155668
Victor Manzanilla <vmanzanilla7@gmail.com>
2020-07-27 00:16:51 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,4 +1,10 @@
 def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(k, v), new_array| new_array
+
+  v.each do |inner_k, names| 
+    
+    names.each do |name|
+    end
+end
 end
 end
